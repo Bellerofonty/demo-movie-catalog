cf72fce716c740c60effbd815e36f9980734dea1
Bellerofonty <34703047+Bellerofonty@users.noreply.github.com>
2024-05-01 16:23:09 +0300

    movie

--- src/pages/movie/Movie.tsx
+++ src/pages/movie/Movie.tsx
@@ -1,53 +1,14 @@
-import {MoviesList} from "../../components/MoviesList";
-import React, {useEffect, useReducer, useState} from "react";
+import React, {useEffect, useReducer} from "react";
 import * as moviesData from "../../moviesData.json"
 import {MovieCard} from "../../components/MovieCard";
 import {useParams, Params} from "react-router-dom"
 import {IMovieData} from "../../models";
-import * as token from '../../APIToken.json'
-import axios, {AxiosError} from "axios";
 import {Loader} from "../../components/Loader";
-
-type MovieId = number | 'random'
-
-interface IGetMovieDataParams {
-    id: MovieId
-}
-
-interface IResponseData {
-    docs?: IMovieData[]
-}
+import {getMovieData, MovieId} from "./request";
 
 const {docs} = moviesData
-const {APIToken} = token
-const fields = ['id', 'name', 'rating', 'genres', 'description', 'poster', 'year', 'movieLength']
 
-const getMovieData = ({id}: IGetMovieDataParams): Promise<IMovieData> => {
-    const options = {
-        method: 'GET',
-        params: {
-            page: '1',
-            limit: '1',
-            selectFields: fields,
-            notNullFields: fields,
-            type: 'movie',
-            id: id === "random" ? undefined : id
-        },
-        headers: {accept: 'application/json', 'X-API-KEY': APIToken},
-        url: id === "random" ? 'https://api.kinopoisk.dev/v1.4/movie/random' : 'https://api.kinopoisk.dev/v1.4/movie',
-    }
-
-    return axios.request(options)
-        .then(({data}: {data: IResponseData}) => {
-            if (!data || !data.docs || !data.docs[0]) {
-                throw new Error('No data')
-            }
-
-            return data.docs[0]
-    })
-}
-
-const parseIdFromParams = (params: Params<string>): MovieId => {
+const parseIdFromParams = (params: Params): MovieId => {
     const {id} = params;
     if (!id) {
         return 'random'
---
+++ src/pages/movie/const.ts
@@ -0,0 +1,3 @@
+export const FIELDS = ['id', 'name', 'rating', 'genres', 'description', 'poster', 'year', 'movieLength'] as const;
+export const RANDOM_MOVIE_URL = 'https://api.kinopoisk.dev/v1.4/movie/random' as const;
+export const MOVIE_URL = 'https://api.kinopoisk.dev/v1.4/movie' as const;
